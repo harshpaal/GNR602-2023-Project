8fac4097f12cadefec9299bf9c814dd46497615d
Harsh Pal <pal.harsh@siemens.com>
2023-04-16 06:21:01 +0530

    Updated utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,27 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def cluster_and_plot(n_clusters, model, pixels, img):
-    agglo = model(k=n_clusters, initial_k=25)
-    agglo.fit(pixels)
-
-    new_img = [[agglo.predict_center(pixel) for pixel in row] for row in img]
-    new_img = np.array(new_img, np.uint8)
-
-    plt.figure(figsize=(15,15))
-
-    plt.subplot(1,2,1)
-    plt.imshow(img)
-    plt.axis('off')
-    plt.title('Original image')
-
-    plt.subplot(1,2,2)
-    plt.imshow(new_img)
-    plt.axis('off')
-    plt.title(f'Segmented image with k={n_clusters}')
-
-    plt.show()
-
 def euclidean_distance(point1, point2):
     """
     Computes euclidean distance of point1 and point2.
